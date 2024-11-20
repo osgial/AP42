1530ca85cad65367aa5d96404d506b773db7b734
osgial <osgial@alumnatflorida.es>
2024-11-20 17:43:31 +0100

    Add files via upload

---
+++ Ej4.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>AP42OscarGiner</title>
+</head>
+<body>
+    
+    <h1>AP42OscarGiner</h1>
+
+    <?php
+    $array1=[0,1,2,2,3,4,5,5,6,7,8,9,9,10];
+    $pares=[];
+    $impares=[];
+
+    foreach ($array1 as $valor) {
+        if ($valor % 2 == 0) {
+            $pares[]=$valor;
+        } else {
+            $impares[]=$valor;
+        }
+    }
+
+    var_dump($array1);
+    $numeros[] = $pares;
+    $numeros[] = $impares;
+    var_dump($numeros);
+    ?>
+
+</body>
+</html>
